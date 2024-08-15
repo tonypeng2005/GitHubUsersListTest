a2834410dcb5dd0796d2b94095130276307daa68
Tony Peng <tonypeng2005@hotmail.com>
2024-08-15 16:35:45 +0800

    Initial Commit

---
+++ GitHubUsersListTest/GitHubUsersListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersListTestApp.swift
+//  GitHubUsersListTest
+//
+//  Created by Tony Peng on 2024/8/15.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
